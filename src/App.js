3a3f9a7da005ffc2a64fb61012ab6a6943cffa8d
BeaO <owusubea@gmail.com>
2022-09-04 16:35:35 +0000

    updated react
    	new file:   public/bootstrap.bundle.js
    	new file:   public/calculator
    	new file:   public/calculator.css
    	new file:   public/css/bootstrap-grid.css
        new file:   public/css/bootstrap-grid.css.map
    	new file:   public/css/bootstrap-grid.min.css
    	new file:   public/css/bootstrap-grid.min.css.map
    	new file:   public/css/bootstrap-reboot.css
    	new file:   public/css/bootstrap-reboot.css.map
    	new file:   public/css/bootstrap-reboot.min.css
    	new file:   public/css/bootstrap-reboot.min.css.map
    	new file:   public/css/bootstrap.css
    	new file:   public/css/bootstrap.css.map
    	new file:   public/css/bootstrap.min.css
    	new file:   public/css/bootstrap.min.css.map
    	modified:   public/index.html
    	new file:   public/indexc.html
    	new file:   public/jquery-3.6.0.min.js
    	modified:   src/App.js
    	modified:   public/calculator (modified content)

--- src/App.js
+++ src/App.js
@@ -3,22 +3,51 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+<div className="container mainc">
+	<div className="row r1">
+		<div className="col-md-12">0</div>
+	</div>
+
+	<div className="row r2">
+		<div className="col-md-3 AC"><h3>AC</h3></div>
+		<div className="col-md-3 pm"><h3>+/-</h3></div>
+		<div className="col-md-3 per"><h3>%</h3></div>
+		<div className="col-md-3 divide"><h3>&#247;</h3></div>
+	</div>
+
+	<div className="row r3">
+		<div className="col-md-3 sev"><h3>7</h3></div>
+		<div className="col-md-3 eig"><h3>8</h3></div>
+		<div className="col-md-3 nin"><h3>9</h3></div>
+		<div className="col-md-3 multiply"><h3 class="x">&#215;</h3></div>
+	</div>
+
+	<div className="row r4">
+		<div className="col-md-3 fou"><h3>4</h3></div>
+		<div className="col-md-3 fiv"><h3>5</h3></div>
+		<div className="col-md-3 six"><h3>6</h3></div>
+		<div className="col-md-3 minus"><h3>-</h3></div>
+	</div>
+
+	<div className="row r5">
+		<div className="col-md-3 one"><h3>1</h3></div>
+		<div className="col-md-3 two"><h3>2</h3></div>
+		<div className="col-md-3 thr"><h3>3</h3></div>
+		<div className="col-md-3 pl"><h3>+</h3></div>
+	</div>
+
+	<div className="row r6">
+		<div className="col-md-6 zer"><h3>0</h3></div>
+		<div className="col-md-3 dot"><h3>.</h3></div>
+		<div className="col-md-3 eq"><h3>=</h3></div>
+	</div> 
+	
+</div>
+
+
+</>
   );
 }
 
